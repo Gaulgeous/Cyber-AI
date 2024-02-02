d3435f029d44833be44c156be73faf67bc4ace84
gaulgeous <gauldavid78259@gmail.com>
2024-02-02 11:29:15 +0800

    Added full functionality for data cleaning, margin dropping and regression implementations. Might as well submit this thing

--- cyber_ai/genetic_algorithm.py
+++ cyber_ai/genetic_algorithm.py
@@ -9,39 +9,57 @@ from copy import copy
 from math import exp
 from joblib import parallel, delayed
 
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.svm import LinearSVC
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, ElasticNet
+from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score,confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score,confusion_matrix, ConfusionMatrixDisplay, mean_squared_error as mse, mean_absolute_error as mae, mean_absolute_percentage_error as mape, r2_score as r2
 
 
 
-models = {"knn": {'n_neighbors': range(1, 101), 'weights': ["uniform", "distance"], 'p': [1, 2]},
+classification_models = {"knn": {'n_neighbors': range(1, 101), 'weights': ["uniform", "distance"], 'p': [1, 2]},
           'svc': {'penalty': ["l2"], 'loss': ["hinge", "squared_hinge"], 'dual': [True], 'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1], 'C': [1e-4, 1e-3, 1e-2, 1e-1, 0.5, 1., 5., 10., 15., 20., 25.]},
           'logistic': {'penalty': ["l2"], 'C': [1e-4, 1e-3, 1e-2, 1e-1, 0.5, 1., 5., 10., 15., 20., 25.], 'dual': [False]},
           'rf': {'n_estimators': [100], 'criterion': ["gini", "entropy"], 'max_features': np.arange(0.05, 1.01, 0.05), 'min_samples_split': range(2, 21), 'min_samples_leaf':  range(1, 21), 'bootstrap': [True, False]},
           'decision_tree': {'criterion': ["gini", "entropy"], 'max_depth': range(1, 11), 'min_samples_split': range(2, 21), 'min_samples_leaf': range(1, 21)}
         }
 
+regression_models = {"rf": {'n_estimators': [100], 'max_features': np.arange(0.05, 1.01, 0.05), 'min_samples_split': range(2, 21), 'min_samples_leaf': range(1, 21), 'bootstrap': [True, False]},
+                     "knn": {'n_neighbors': range(1, 101), 'weights': ["uniform", "distance"], 'p': [1, 2]},
+                     "decision_tree": {'max_depth': range(1, 11), 'min_samples_split': range(2, 21), 'min_samples_leaf': range(1, 21)},
+                     "elastic": {'l1_ratio': np.arange(0.0, 1.01, 0.05), 'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]}
+                     }
+
+cleaners = {"scaler": ["minmax", "robust", "standard", "none"], "pca": ["none", "pca"]}
+
 
 class GeneticAlgorithm:
 
-    def __init__(self, model, population=20, generations=20, cv=5, parents=4, scoring='f1'):
+    def __init__(self, model, cols, mode="classification", population=20, generations=20, cv=5, parents=4):
 
         self.model = model
+        self.cols = cols
+        self.mode = mode
         self.population_size = population
         self.generations = generations
         self.cv = cv
-        self.scoring = scoring
         self.parents = parents
         self.population = []
         self.X_train = None
         self.y_train = None
         self.best_genome = None
+        self.models = None
+
+        if mode == "classification":
+            self.models = classification_models
+            self.scoring = "f1"
+        else:
+            self.models = regression_models
+            self.scoring = "r2"
 
         self.create_population()
 
@@ -49,28 +67,53 @@ class GeneticAlgorithm:
     def create_genome(self):
 
         genome = {}
-        for key in models[self.model]:
-            value = choices(models[self.model][key], k=1)[0]
+        clean = {}
+
+        for key in self.models[self.model]:
+            value = choices(self.models[self.model][key], k=1)[0]
             genome[key] = value
 
-        return genome
+        for cleaner in cleaners:
+            value = choices(cleaners[cleaner], k=1)[0]
+            clean[cleaner] = value
+
+        drop_margins = np.array([randint(0, 1) for _ in range(self.cols)])
+        if sum(drop_margins) < 2:
+            a = randint(0, len(drop_margins) - 1)
+            b = randint(0, len(drop_margins) - 1)
+            while b == a:
+                b = randint(0, len(drop_margins) - 1)
+            drop_margins[a] = 1
+            drop_margins[b] = 1
+            drop_margins = np.asarray(drop_margins)
+
+        return {"drop_margins": drop_margins, "cleaners": clean, "model": genome}
 
 
     def create_model(self, genome):
 
         model = None
 
-        if self.model == "knn":
-            model = KNeighborsClassifier(n_neighbors=genome['n_neighbors'], weights=genome['weights'], p=genome['p'])
-        elif self.model == "svc":
-            model = LinearSVC(penalty=genome["penalty"], loss=genome["loss"], dual=genome["dual"], tol=genome["tol"], C=genome["C"])
-        elif self.model == "logistic":
-            model = LogisticRegression(penalty=genome["penalty"], C=genome["C"], dual=genome["dual"])
-        elif self.model == "rf":
-            model = RandomForestClassifier(n_estimators=genome["n_estimators"], criterion=genome["criterion"], max_features=genome["max_features"], min_samples_split=genome["min_samples_split"], min_samples_leaf=genome["min_samples_leaf"], bootstrap=genome["bootstrap"])
-        elif self.model == "decision_tree":
-            model = DecisionTreeClassifier(criterion=genome["criterion"], max_depth=genome["max_depth"], min_samples_split=genome["min_samples_split"], min_samples_leaf=genome["min_samples_leaf"])
-      
+        if self.mode == "classification":
+            if self.model == "knn":
+                model = KNeighborsClassifier(n_neighbors=genome['n_neighbors'], weights=genome['weights'], p=genome['p'])
+            elif self.model == "svc":
+                model = LinearSVC(penalty=genome["penalty"], loss=genome["loss"], dual=genome["dual"], tol=genome["tol"], C=genome["C"])
+            elif self.model == "logistic":
+                model = LogisticRegression(penalty=genome["penalty"], C=genome["C"], dual=genome["dual"])
+            elif self.model == "rf":
+                model = RandomForestClassifier(n_estimators=genome["n_estimators"], criterion=genome["criterion"], max_features=genome["max_features"], min_samples_split=genome["min_samples_split"], min_samples_leaf=genome["min_samples_leaf"], bootstrap=genome["bootstrap"])
+            elif self.model == "decision_tree":
+                model = DecisionTreeClassifier(criterion=genome["criterion"], max_depth=genome["max_depth"], min_samples_split=genome["min_samples_split"], min_samples_leaf=genome["min_samples_leaf"])
+        else:
+            if self.model == "rf":
+                model = RandomForestRegressor(n_estimators=genome["n_estimators"], max_features=genome["max_features"], min_samples_split=genome["min_samples_split"], min_samples_leaf=genome["min_samples_leaf"], bootstrap=genome["bootstrap"])
+            elif self.model == "decision_tree":
+                model = DecisionTreeRegressor(max_depth=genome["max_depth"], min_samples_split=genome["min_samples_split"], min_samples_leaf=genome["min_samples_leaf"])
+            elif self.model == "elastic":
+                model = ElasticNet(l1_ratio=genome["l1_ratio"], tol=genome["tol"])
+            elif self.model == "knn":
+                model = KNeighborsRegressor(n_neighbors=genome["n_neighbors"], weights=genome["weights"], p=genome["p"])
         return model
 
 
@@ -97,7 +140,13 @@ class GeneticAlgorithm:
                 parents.extend(mutations)
                 self.population = parents
             else:
+                if generation % 5 == 0:
+                    print()
+                    print(f"generation {generation}")
                 self.best_genome = sorted_population[0]
+                print("best drop_margins {0}".format(self.best_genome["drop_margins"]))
+                print("best cleaners {0}".format(self.best_genome["cleaners"]))
+                print("best model {0}".format(self.best_genome["model"]))
 
             
     def create_parents(self, population):
@@ -105,7 +154,11 @@ class GeneticAlgorithm:
 
         while len(parents) < self.parents:
             new_parent = choices(population, k=1, weights=[exp(-x*0.1) for x in range(self.population_size)])[0]
-            if new_parent not in parents:
+            present = 0
+            for parent in parents:
+                if np.array_equal(new_parent["drop_margins"], parent["drop_margins"]) and new_parent["cleaners"] == parent["cleaners"] and new_parent["model"] == parent["model"]:
+                    present = 1
+            if not present:
                 parents.append(new_parent)
 
         return parents
@@ -122,19 +175,46 @@ class GeneticAlgorithm:
             parent_a = parents[pair[0]]
             parent_b = parents[pair[1]]
 
-            child_a = {}
-            child_b = {}
-
             alternator = 0
 
-            for key in parent_a:
+            drop_margins_a = []
+            drop_margins_b = []
+
+            cleaners_a = {}
+            cleaners_b = {}
+
+            model_a = {}
+            model_b = {}
+
+            for i in range(len(parent_a["drop_margins"])):
                 if alternator:
-                    child_a[key] = parent_a[key]
-                    child_b[key] = parent_b[key]
+                    drop_margins_a.append(parent_a["drop_margins"][i])
+                    drop_margins_b.append(parent_b["drop_margins"][i])
                 else:
-                    child_a[key] = parent_b[key]
-                    child_b[key] = parent_a[key]
+                    drop_margins_a.append(parent_b["drop_margins"][i])
+                    drop_margins_b.append(parent_a["drop_margins"][i])
                 alternator = not alternator
+
+            for cleaner in parent_a["cleaners"]:
+                if alternator:
+                    cleaners_a[cleaner] = parent_a["cleaners"][cleaner]
+                    cleaners_b[cleaner] = parent_b["cleaners"][cleaner]
+                else:
+                    cleaners_a[cleaner] = parent_b["cleaners"][cleaner]
+                    cleaners_b[cleaner] = parent_a["cleaners"][cleaner]
+                alternator = not alternator
+
+            for key in parent_a["model"]:
+                if alternator:
+                    model_a[key] = parent_a["model"][key]
+                    model_b[key] = parent_b["model"][key]
+                else:
+                    model_a[key] = parent_b["model"][key]
+                    model_b[key] = parent_a["model"][key]
+                alternator = not alternator
+
+            child_a = {"drop_margins": drop_margins_a, "cleaners": cleaners_a, "model": model_a}
+            child_b = {"drop_margins": drop_margins_b, "cleaners": cleaners_b, "model": model_b}
 
             crossovers.append(child_a)
             crossovers.append(child_b)
@@ -142,22 +222,32 @@ class GeneticAlgorithm:
         return crossovers
     
 
-    def create_mutations(self, population, n_mutations=1):
+    def create_mutations(self, population, n_mutations=2):
 
         mutations = []
 
         while len(population) + len(mutations) < self.population_size:
             genome = population[randint(0, len(population) - 1)].copy()
 
-            for n in range(n_mutations):
-                index = randint(0, len(genome))
+            for _ in range(n_mutations):
+                segment = randint(0, len(genome)-1)
 
-                if index != len(genome):
-                    key = list(genome.keys())[index]
-                    new_value = choices(models[self.model][key], k=1)[0]
-                    genome[key] = new_value
+                if segment == 0:
+                    index = randint(0, len(genome["drop_margins"]) - 1)
+                    genome["drop_margins"][index] = not genome["drop_margins"][index]
 
-                # Put in use case for margin dropping here
+                elif segment == 1:
+                    index = randint(0, len(genome["cleaners"]) - 1)
+                    key = list(genome["cleaners"].keys())[index]
+                    new_value = choices(cleaners[key], k=1)[0]
+                    genome["cleaners"][key] = new_value
+
+                elif segment == 2:
+                    index = randint(0, len(genome["model"]) - 1)
+                    key = list(genome["model"].keys())[index]
+                    new_value = choices(self.models[self.model][key], k=1)[0]
+                    genome["model"][key] = new_value
+
                     
             mutations.append(genome)
 
@@ -184,46 +274,110 @@ class GeneticAlgorithm:
 
 
     def calc_fitness(self, genome):
-        model = self.create_model(genome)
-        scores = cross_val_score(estimator=model, X=self.X_train, y=self.y_train, cv=self.cv, scoring=self.scoring)
+        
+        X_train = np.asarray(self.X_train)
+        X_train = X_train[:,np.asarray(genome["drop_margins"]).astype('bool')]
+        
+        if genome["cleaners"]["scaler"] == "robust":
+            scaler = RobustScaler()
+            X_train = scaler.fit_transform(X_train)
+        elif genome["cleaners"]["scaler"] == "standard":
+            scaler = StandardScaler()
+            X_train = scaler.fit_transform(X_train)
+        elif genome["cleaners"]["scaler"] == "minmax":
+            scaler = MinMaxScaler()
+            X_train = scaler.fit_transform(X_train)
+
+        if genome["cleaners"]["pca"] == "pca":
+            pca = PCA(n_components="mle")
+            X_train = pca.fit_transform(X_train)
+
+        model = self.create_model(genome["model"])
+
+        scores = cross_val_score(estimator=model, X=X_train, y=self.y_train, cv=self.cv, scoring=self.scoring)
         return mean(scores)
     
 
     def predict(self, X_test, y_test):
 
-        model = self.create_model(self.best_genome)
-        model.fit(self.X_train, self.y_train)
+        X_train = np.asarray(self.X_train)
+        X_test = np.asarray(X_test)
+        X_train = X_train[:,np.asarray(self.best_genome["drop_margins"]).astype('bool')]
+        X_test = X_test[:,np.asarray(self.best_genome["drop_margins"]).astype('bool')]
+        
+        if self.best_genome["cleaners"]["scaler"] == "robust":
+            scaler = RobustScaler()
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.fit_transform(X_test)
+        elif self.best_genome["cleaners"]["scaler"] == "standard":
+            scaler = StandardScaler()
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.fit_transform(X_test)
+        elif self.best_genome["cleaners"]["scaler"] == "minmax":
+            scaler = MinMaxScaler()
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.fit_transform(X_test)
+
+        if self.best_genome["cleaners"]["pca"] == "pca":
+            pca = PCA(n_components="mle")
+            X_train = pca.fit_transform(X_train)
+            X_test = pca.fit_transform(X_test)
+
+        model = self.create_model(self.best_genome["model"])
+
+        model.fit(X_train, self.y_train)
         predictions = model.predict(X_test)
 
-        accuracy = accuracy_score(y_test, predictions)
-        f1 = f1_score(y_test, predictions)
-        recall = recall_score(y_test, predictions)
-        precision = precision_score(y_test, predictions)
+        if self.mode == "classification":
 
-        print()
-        print()
-        print(f"Final Accuracy: {accuracy}")
-        print(f"Final f1: {f1}")
-        print(f"Final recall: {recall}")
-        print(f"Final precision: {precision}")
+            accuracy = accuracy_score(y_test, predictions)
+            f1 = f1_score(y_test, predictions)
+            recall = recall_score(y_test, predictions)
+            precision = precision_score(y_test, predictions)
 
-        cm = confusion_matrix(y_test, predictions)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-        disp.plot()
-        plt.show()
+            print()
+            print()
+            print(f"Final Accuracy: {accuracy}")
+            print(f"Final f1: {f1}")
+            print(f"Final recall: {recall}")
+            print(f"Final precision: {precision}")
+
+            cm = confusion_matrix(y_test, predictions)
+            disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+            disp.plot()
+            plt.show()
+
+        elif self.mode == "regression":
+
+            RMSE = mse(y_test, predictions, squared=False)
+            MAE = mae(y_test, predictions)
+            MAPE = mape(y_test, predictions)
+            R2 = r2(y_test, predictions)
+
+            print()
+            print()
+            print(f"Final RMSE: {RMSE}")
+            print(f"Final MAE: {MAE}")
+            print(f"Final MAPE: {MAPE}")
+            print(f"Final R2: {R2}")
 
 
 if __name__=='__main__':
 
-    data_path = r"/home/david/Documents/Cyber-AI/data/Training Dataset.csv"
+    data_path = r"/home/david/Documents/Cyber-AI/data/Student_Performance.csv"
     df = pd.read_csv(data_path)
 
-    labels = df["Result"]
-    data = df.drop("Result", axis=1)
+    mapping = {'Yes': 1, 'No': 0}
+    column = df['Extracurricular Activities'].map(mapping)
+    df['Extracurricular Activities'] = column
+
+    labels = df["Performance Index"]
+    data = df.drop("Performance Index", axis=1)
 
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.25, random_state=42)
 
-    genetic_algorithm = GeneticAlgorithm("decision_tree", population=20, generations=20, cv=2)
+    genetic_algorithm = GeneticAlgorithm("decision_tree", cols=X_train.shape[1], mode="regression", parents=4, population=20, generations=20, cv=2)
     genetic_algorithm.fit(X_train, y_train)
+    print("predicting")
     genetic_algorithm.predict(X_test, y_test)
 
